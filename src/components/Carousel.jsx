1dbe129bd9b3c06debc37c12f2112873a8e92611
ramirogz1 <ramirogz101@gmail.com>
2024-06-11 19:21:18 -0300

    proyecto terminado

--- src/components/Carousel.jsx
+++ src/components/Carousel.jsx
@@ -5,13 +5,13 @@ export const Carousel = () => {
     <div id="carouselExampleAutoplaying" className="carousel slide" data-bs-ride="carousel">
   <div className="carousel-inner">
     <div className="carousel-item active">
-      <img src="https://stjohns.mx/wp-content/uploads/2021/06/Los-mejores-juegos-para-nin%CC%83os-1-1200x675.png" className="d-block w-100" alt="..." style={{maxHeight: "550px"}}/>
+      <img src="src/images/nenes1.png" className="d-block w-100" alt="..." style={{maxHeight: "550px"}}/>
     </div>
     <div className="carousel-item">
-      <img src="https://inoplay.com.mx/juegos-infantiles/fotos/juegos-modulares-de-exterior/juegos-de-exterior-para-40-usuarios.jpg" className="d-block w-100" alt="..." style={{maxHeight: "550px"}}/>
+      <img src="src/images/nenesJugando.png" className="d-block w-100" alt="..." style={{maxHeight: "550px"}}/>
     </div>
     <div className="carousel-item">
-      <img src="https://inoplay.com.mx/juegos-infantiles/fotos/juegos-modulares-de-exterior/islas-de-juegos-modulares-20-usuarios.jpg" className="d-block w-100" alt="..." style={{maxHeight: "55ñ0px"}}/>
+      <img src= "src/images/nenes2.png" className="d-block w-100" alt="..." style={{maxHeight: "550px"}}/>
     </div>
   </div>
   <button className="carousel-control-prev" type="button" data-bs-target="#carouselExampleAutoplaying" data-bs-slide="prev">
